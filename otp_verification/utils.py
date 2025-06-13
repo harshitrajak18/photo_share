8a844e23308a5c1d3f5155afb4c58567293ed56b
Harshit Rajak <106432378+harshitrajak18@users.noreply.github.com>
2025-06-13 20:41:04 +0530

    Update utils.py

--- otp_verification/utils.py
+++ otp_verification/utils.py
@@ -1,10 +1,10 @@
 from django.core.mail import send_mail
 from django.conf import settings
-from celery import shared_task
+#from celery import shared_task
 
 
 
-@shared_task
+#@shared_task
 def send_email_to_client(email,otp):
     
     subject="OTP for verification"
